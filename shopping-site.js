e19537411a390ccd4c15eb1a1c71d5ff51188e18
eitotoyoda <tydeight@yahoo.co.jp>
2017-06-04 16:58:10 +0000

    PC_Complete

--- shopping-site.js
+++ shopping-site.js
@@ -16,3 +16,10 @@ $(document).ready(function(){
     return false;
   });
 });
+
+$(window).load(function(){
+  $("#list_img img").click(function(){
+    var img_src = $(this).attr("src");
+    $("#display_img img").attr("src", img_src);
+  });
+});
